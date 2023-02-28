b32513bb498390711178b761380db5caa6badb50
Roxana Bita <roxana.bita@gmail.com>
2023-02-28 09:44:20 +0100

    set preorder condition

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const products_1 = require("./products");
 const productName = 'fanny pack';
-const product = products_1.default.filter(product => product.name === productName[0]);
-console.log(product);
+const product = products_1.default.filter(product => {
+    if (product.name === productName) {
+        if (product.preOrder === true) {
+            console.log("Thank you for your pre-order! We'll let you know as soon as we ship your items.");
+        }
+    }
+});
--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import products from "./products";
 
 const productName:string='fanny pack';
-const product= products.filter(product=>product.name === productName[0]);
-console.log(product);
+const product = products.filter(product => {
+    if (product.name === productName) { 
+      if (product.preOrder === true) { 
+        console.log("Thank you for your pre-order! We'll let you know as soon as we ship your items.")
+      } } }
+   )
