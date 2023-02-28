5557ed7a15e92dccce398b6299e8f285d32131d1
Roxana Bita <roxana.bita@gmail.com>
2023-02-28 13:15:54 +0100

    completed

--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
 import products from "./products";
 
-const productName:string='fanny pack';
-const product = products.filter(product => {
-    if (product.name === productName) { 
-      if (product.preOrder === true) { 
-        console.log("Thank you for your pre-order! We'll let you know as soon as we ship your items.")
-      } } }
-   )
+const productName: string = "beanie";
+let shipping: number = 5;
+let taxPercent: number = 0.05;
+let total: number;
+const shippingAddress: string = "5th Avenue New York";
+
+const product = products.find((product) => {
+  return product.name === productName;
+});
+
+console.log(product);
+
+if (product) {
+  if (product.preOrder === true) {
+    console.log(
+      "Thank you for your pre-order! We'll let you know as soon as we ship your items."
+    );
+  }
+
+  if (Number(product.price) >= 25) {
+    shipping = 0;
+  }
+
+  if (shippingAddress.includes("New York")) {
+    taxPercent = 0.1;
+  }
+
+  total = taxPercent * Number(product.price) + Number(product.price) + shipping;
+  console.log(product.name, shippingAddress, shipping , product.price, taxPercent, total );
+} else console.log("no product found");
